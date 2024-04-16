a14cb384c6881869c9b4efb29c5a156130ed66ca
apenduks1 <k77154313@gmail.com>
2024-04-16 18:33:19 +0300

    first commit

---
+++ asets/js/custom.js
@@ -0,0 +1,5 @@
+document.querySelector(".comments--coment1--add").addEventListener("click", function() {
+
+  document.querySelector(".comments--test").classList.remove(".comments--test1");
+  document.querySelector(".comments--test").classList.remove(".comments--test");
+  });
